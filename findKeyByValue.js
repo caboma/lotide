ebf31a5c291104b3daae274c0e347bba4a747da6
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-04 01:38:07 +0000

    Initial Commit

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const findKeyByValue = function (tvShowsGenre, title){
+let showsGenre = Object.keys(tvShowsGenre)
+  for (let genre of showsGenre){
+    if(tvShowsGenre[genre] === title){
+      return genre;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
