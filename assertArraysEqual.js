91586db575d27386a91b90150020c106916ff29c
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-08 18:42:50 +0000

    refactored using modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,7 @@
-const eqArrays = function (array1, array2){
-  let result = '';
-  for (let i =0; i < array1.length; i++){
-    if (array1[i] === array2[i]) {
-      result = true;
-    }
-    else {
-      result = false;
-    }
-  }
-  return result;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (input1, input2){
   console.log(eqArrays(input1, input2));
 }
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = assertArraysEqual;
