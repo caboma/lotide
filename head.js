ef2e659cc6ae62bafded330c4d8406e6a001df04
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-08 18:21:53 +0000

    refactored using modules

--- head.js
+++ head.js
@@ -1,17 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(myArray) {
   return myArray[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6]), 5);
-assertEqual(head([]), 5);
+module.exports = head;
