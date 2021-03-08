ae8210a067960aef937849438fcb4e0c83636505
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-08 20:56:35 +0000

    refactored using Mocha and Chi

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,29 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6]), 5);
-assertEqual(head([]), 5);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([6]), 5);
+// assertEqual(head([]), 5);
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  
+  it("should return 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it("should return Hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), "Hello");
+  });
+
+  it("should return 6 for [6]", () => {
+    assert.strictEqual(head([6]), 6);
+  });
+
+  it("should return underfined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
