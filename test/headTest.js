8c2cacb3f3350869143e18bdbabb26f7777c21e1
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-08 20:58:58 +0000

    refactored using Mocha and Chi

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,3 @@
-// const head = require('../head');
-// const assertEqual = require('../assertEqual');
-
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([6]), 5);
-// assertEqual(head([]), 5);
-
 const assert = require('chai').assert;
 const head   = require('../head');
 
