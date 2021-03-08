b3b8f175d505df184efb37870beacfc82024534c
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-08 18:24:29 +0000

    refactored using modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -20,11 +20,7 @@ const eqArrays = function (array1, array2){
   }
   return true;
 } 
+module.exports = eqArrays;
 
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
