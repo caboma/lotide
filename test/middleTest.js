41323cbbc33cd0ce3bf9204c1b5cf35ba1a8da14
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-08 18:48:25 +0000

    refactored using modules

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+middle([1]) // => []
+middle([1, 2]) // => []
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
